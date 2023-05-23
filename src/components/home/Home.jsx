d24780d3877da40878a9e35ea95cddf5d77f54a6
Bhargav Gajjar <bhargavgajjar10@gmail.com>
2023-05-23 23:54:37 +0530

    feat: added main home page design with cards

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react";
 import logo from "../../assets/images/logo.svg";
+import avatar from "../../assets/images/icons/avatar.svg";
+import notification from "../../assets/images/icons/notification.svg";
 import plusIcon from "../../assets/images/icons/plus.svg";
 import "./_home.scss";
 import { CSSTransition } from "react-transition-group";
@@ -31,6 +33,26 @@ const Home = () => {
             <span key={items} className={"color-" + items}></span>
           ))}
         </div>
+      </div>
+      <div className="content-right">
+        <nav>
+          <div className="search"></div>
+          <input type="text" name="search" id="search" placeholder="Search" />
+          <div className="notes-profile">
+            <img src={notification} alt="" />
+            <img src={avatar} alt="" />
+          </div>
+        </nav>
+        <section className="home">
+          <p className="title-home">Notes</p>
+          <div className="cards-parent">
+            {[1, 2, 3, 4, 5].map((items) => (
+              <div key={items} className={"cards-custom color-" + items}>
+                <p>Whereas recognition of the inherent dignity</p>
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
     </div>
   );
